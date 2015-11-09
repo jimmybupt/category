29ccf68db522240e01f1e5493a1699c79f9dc869
EmmaR-OSU <rastatter.6@osu.edu>
2015-11-09 16:46:54 -0500

    Split JS out of HTML

---
+++ addProductCategory.js
@@ -0,0 +1,48 @@
+function printTable() {
+
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("tableView").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","viewTable.php",true);
+        xmlhttp.send();
+}
+
+function deleteRow() {
+	var uname = document.getElementById("deleteUName").value;
+	if(uname == "") {
+		return;
+	}
+	if (window.XMLHttpRequest) {
+	   xmlhttp = new XMLHttpRequest();
+	} else {
+	 	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	   xmlhttp.open("GET","deleteRow.php?q="+uname,true);
+		xmlhttp.send();
+		printTable();
+}
+
+function addRow() {
+	var uname = document.getElementById("addUName").value;
+	var cname = document.getElementById("addCName").value;
+	if(uname == "" || cname == "") {
+		return;
+	}
+	if (window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.open("GET", "addRow.php?q="+uname+"&r="+cname,true);
+	xmlhttp.send();
+	printTable();
+}
